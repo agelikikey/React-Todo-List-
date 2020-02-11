084ca8c4bed4c1b8e9ab82b8cade7e757c2b8e9c
agelikikey <agelikikey@gmail.com>
2020-02-11 01:14:54 +0000

    My First ToDo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [list, setlist] = useState([]);
+	const [content, setContent] = useState();
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="text-center mt-5 pt-5">
+				<h1>ToDo List!</h1>
+                <input 
+                value={content}
+                onChange={e => setContent(e.target)} 
+                oneKeyPress={e => {
+                    if (e.key === "Enter") {
+                            setlist(list.concat(content));
+                            setContent("Add a Task");
+                    }
+                }}
+                />
+				{list.map((item, index) => {
+					return <div key={index}>{item}</div>;
+				})}
+			</div>
+		</>
 	);
 }
